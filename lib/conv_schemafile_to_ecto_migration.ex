a4aaf050042bf83419ddbf3092b4d33aca7871c4
Ambe Hideki <ykc@daycrift.net>
2023-01-03 22:13:26 +0900

    mix format mix.exs "lib/**/*.{ex,exs}"

--- lib/conv_schemafile_to_ecto_migration.ex
+++ lib/conv_schemafile_to_ecto_migration.ex
@@ -13,22 +13,27 @@ defmodule ConvSchemafileToEctoMigration do
 
   """
   def gen_migration do
-    text = File.read! "Schemafile"
+    text = File.read!("Schemafile")
 
-    output = text
-    |> String.split("\n")
-    |> Enum.reduce([], fn line, context -> _read_line(line, context) end)
-    |> Enum.map(fn table -> _transform_ecto_create_table(table) end)
-    |> Enum.map_join("\n", &(Enum.join(&1, "\n")))
+    output =
+      text
+      |> String.split("\n")
+      |> Enum.reduce([], fn line, context -> _read_line(line, context) end)
+      |> Enum.map(fn table -> _transform_ecto_create_table(table) end)
+      |> Enum.map_join("\n", &Enum.join(&1, "\n"))
 
     datetime = DateTime.now!("Etc/UTC") |> _migration_format_datetime
     File.write!("#{datetime}_create_tables.exs", _ecto_template(output))
   end
 
-  defp _migration_format_datetime(d), do: "#{d.year}#{_zero_padding(d.month)}#{_zero_padding(d.day)}#{_zero_padding(d.hour)}#{_zero_padding(d.minute)}#{d.second}"
-  defp _zero_padding(i), do: i |> Integer.to_string |> String.pad_leading(2, "0")
+  defp _migration_format_datetime(d),
+    do:
+      "#{d.year}#{_zero_padding(d.month)}#{_zero_padding(d.day)}#{_zero_padding(d.hour)}#{_zero_padding(d.minute)}#{d.second}"
+
+  defp _zero_padding(i), do: i |> Integer.to_string() |> String.pad_leading(2, "0")
 
   defp _base_indent, do: "    "
+
   defp _ecto_template(definition) do
     """
     defmodule MyApi.Repo.Migrations.CreateTables do
@@ -46,20 +51,23 @@ defmodule ConvSchemafileToEctoMigration do
     {:ok, header} = Map.fetch(map, :table_name)
     create_table = "#{_base_indent()}create table(:#{header["table_name"]}) do"
 
-    columns = Enum.filter(map, fn x ->
-      {k, v} = x
-      v[:type] && v[:type] != "index" && k not in [:created_at, :updated_at]
-    end)
-    |> Enum.sort
-    |> Enum.map(fn x ->
-      {name, %{type: type, options: _options}} = x
-      "#{_base_indent()}  add :#{name}, :#{type}"
-    end)
+    columns =
+      Enum.filter(map, fn x ->
+        {k, v} = x
+        v[:type] && v[:type] != "index" && k not in [:created_at, :updated_at]
+      end)
+      |> Enum.sort()
+      |> Enum.map(fn x ->
+        {name, %{type: type, options: _options}} = x
+        "#{_base_indent()}  add :#{name}, :#{type}"
+      end)
 
     # Ecto default timestamps create `inserted_at`
-    timestamps = [:inserted_at, :updated_at] |> Enum.map(fn name ->
-      "#{_base_indent()}  add :#{name}, :utc_datetime"
-    end)
+    timestamps =
+      [:inserted_at, :updated_at]
+      |> Enum.map(fn name ->
+        "#{_base_indent()}  add :#{name}, :utc_datetime"
+      end)
 
     period = "#{_base_indent()}end"
 
@@ -67,27 +75,53 @@ defmodule ConvSchemafileToEctoMigration do
   end
 
   defp _read_line(:eof, context), do: context
+
   defp _read_line(line, context) do
     cond do
-      table_name = Regex.named_captures(~r/create_table +(\'|\")(?<table_name>\w+)(\'|\").*/, line)
-                   -> context ++ [%{table_name: table_name}]
-      matched = Regex.named_captures(~r/ *t\.index +(?<index_columns>.*), +name: +(\'|\")(?<index_name>.*)(\'|\")/, line)
-                -> _conv_index(matched, context)
-      matched = Regex.named_captures(~r/ *t\.(?<type>.*) +(\'|\")(?<column_name>.*)(\'|\"),*(?<options>.*)/, line)
-                -> _conv_column(matched, context)
-     true -> context
+      table_name =
+          Regex.named_captures(~r/create_table +(\'|\")(?<table_name>\w+)(\'|\").*/, line) ->
+        context ++ [%{table_name: table_name}]
+
+      matched =
+          Regex.named_captures(
+            ~r/ *t\.index +(?<index_columns>.*), +name: +(\'|\")(?<index_name>.*)(\'|\")/,
+            line
+          ) ->
+        _conv_index(matched, context)
+
+      matched =
+          Regex.named_captures(
+            ~r/ *t\.(?<type>.*) +(\'|\")(?<column_name>.*)(\'|\"),*(?<options>.*)/,
+            line
+          ) ->
+        _conv_column(matched, context)
+
+      true ->
+        context
     end
   end
 
   defp _conv_index(matched, context) do
     {c, last} = _split_context(context)
-    last = Map.put(last, String.to_atom(matched["index_name"]), %{type: "index", columns: matched["index_columns"]})
+
+    last =
+      Map.put(last, String.to_atom(matched["index_name"]), %{
+        type: "index",
+        columns: matched["index_columns"]
+      })
+
     c ++ [last]
   end
 
   defp _conv_column(matched, context) do
     {c, last} = _split_context(context)
-    last = Map.put(last, String.to_atom(matched["column_name"]), %{type: matched["type"], options: matched["options"]})
+
+    last =
+      Map.put(last, String.to_atom(matched["column_name"]), %{
+        type: matched["type"],
+        options: matched["options"]
+      })
+
     c ++ [last]
   end
 
--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule ConvSchemafileToEctoMigration.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps(),
+      deps: deps()
     ]
   end
 
@@ -21,7 +21,7 @@ defmodule ConvSchemafileToEctoMigration.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
