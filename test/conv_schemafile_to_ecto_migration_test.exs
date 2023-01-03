4460636d8e55d560f057fb0ca6ebd4bb1d803ed7
Ambe Hideki <ykc@daycrift.net>
2023-01-04 01:28:39 +0900

    fix: spec

--- test/conv_schemafile_to_ecto_migration_test.exs
+++ test/conv_schemafile_to_ecto_migration_test.exs
@@ -1,12 +1,39 @@
 defmodule ConvSchemafileToEctoMigrationTest do
   use ExUnit.Case
-  doctest ConvSchemafileToEctoMigration
 
-  setup_all do
-    {:ok, dummy: :dummy}
-  end
+  @test_dir "tmp/for_test"
+  @test_schemafile_path "#{@test_dir}/Schemafile"
 
-  test "gen_migration" do
-    assert ConvSchemafileToEctoMigration.gen_migration() == :ok
+  describe "gen_migration" do
+    setup do
+      current = File.cwd!
+      File.mkdir_p(@test_dir)
+      File.copy!("test/fixtures/Schemafile", @test_schemafile_path)
+      File.cd!(@test_dir)
+
+      on_exit fn ->
+        File.rm_rf(@test_dir)
+        File.cd!(current)
+      end
+
+      {:ok, dummy: :dummy}
+    end
+
+    doctest ConvSchemafileToEctoMigration
+
+    test "create migration file" do
+      assert ConvSchemafileToEctoMigration.gen_migration() == :ok
+
+      {:ok, files} = File.ls()
+      file = files |> Enum.filter(&(&1 =~ ~r/\d+_create_tables.exs/)) |> List.first
+      assert file != nil
+
+      content = File.read!(file)
+      assert String.contains?(content, "create table(:staffs) do")
+      assert String.contains?(content, "add :code, :string")
+      assert String.contains?(content, "add :name, :string")
+      assert String.contains?(content, "add :inserted_at, :utc_datetime")
+      assert String.contains?(content, "add :updated_at, :utc_datetime")
+    end
   end
 end
